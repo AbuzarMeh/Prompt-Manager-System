2d69a651c93a4d1fbea0cfa5025e8ee46f159fb4
Abuzar Mehdi <abuzarmehdi214@gmail.com>
2026-07-09 13:18:58 +0500

    initial commit

---
+++ src/test/java/com/ats/prompt_service/PromptServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ats.prompt_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
