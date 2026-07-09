88cd6a00125c40c8501554f705c59bfeaf1bc47b
Abuzar Mehdi <abuzarmehdi214@gmail.com>
2026-07-09 16:13:32 +0500

    feat(repsitory) : added prompt repository

---
+++ src/main/java/com/ats/prompt_service/repository/PromptRepository.java
@@ -0,0 +1,10 @@
+package com.ats.prompt_service.repository;
+
+import com.ats.prompt_service.entity.Prompt;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface PromptRepository extends JpaRepository<Prompt, UUID> {
+
+}
